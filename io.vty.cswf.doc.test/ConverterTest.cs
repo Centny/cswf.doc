3807d3f58a673d5e52ef06401af4b8ec284462a6
Centny <centny@gmail.com>
2016-02-24 19:09:07 +0800

    adding build script, not finished.

--- io.vty.cswf.doc.test/ConverterTest.cs
+++ io.vty.cswf.doc.test/ConverterTest.cs
@@ -7,7 +7,7 @@ namespace io.vty.cswf.doc.test
     [TestClass]
     public class ConverterTest
     {
-        int OnProcess(Converter.Res res, int count, string spath)
+        int OnProcess(Converter.Res res, int total, int count, string spath)
         {
             res.Files.Add(spath);
             return 1;
@@ -18,12 +18,12 @@ namespace io.vty.cswf.doc.test
         {
             Converter.Res res;
             //
-            res = Converter.word2img(".\\..\\..\\xx.docx", "docx-{0}.png");
+            res = Converter.word2img("xx.docx", "docx-{0}.png");
             Assert.AreEqual(0, res.Code);
             Assert.AreEqual(7, res.Count);
             res.Save("docx0.json");
             //
-            res = Converter.word2img(".\\..\\..\\xx.docx", "docx-{0}.png", 0, true, this.OnProcess);
+            res = Converter.word2img("xx.docx", "docx-{0}.png", 0, true, this.OnProcess);
             Assert.AreEqual(0, res.Code);
             Assert.AreEqual(7, res.Count);
             res.Save("docx1.json");
@@ -33,14 +33,14 @@ namespace io.vty.cswf.doc.test
         {
             Converter.Res res;
             //
-            res = Converter.excel2pdf(".\\..\\..\\xx.xlsx", "xlsx-{0}.pdf");
+            res = Converter.excel2pdf("xx.xlsx", "xlsx-{0}.pdf");
             Assert.AreEqual(0, res.Code);
-            Assert.AreEqual(2, res.Count);
+            Assert.AreEqual(3, res.Count);
             res.Save("xlsx0.json");
             //
-            res = Converter.excel2pdf(".\\..\\..\\xx.xlsx", "xlsx-{0}.pdf", 0, true, this.OnProcess);
+            res = Converter.excel2pdf("xx.xlsx", "xlsx-{0}.pdf", 0, true, this.OnProcess);
             Assert.AreEqual(0, res.Code);
-            Assert.AreEqual(2, res.Count);
+            Assert.AreEqual(3, res.Count);
             res.Save("xlsx1.json");
         }
         [TestMethod]
@@ -49,12 +49,12 @@ namespace io.vty.cswf.doc.test
             //new Converter().excel2pdf("C:\\xxx\\xx.xlsx", "C:\\xxx\\xx-{0}.pdf", true);
             Converter.Res res;
             //
-            res = Converter.ppt2img(".\\..\\..\\xx.pptx", "ppt-{0}.png");
+            res = Converter.ppt2img("xx.pptx", "ppt-{0}.png");
             Assert.AreEqual(0, res.Code);
             Assert.AreEqual(1, res.Count);
             res.Save("ppt0.json");
             //
-            res = Converter.ppt2img(".\\..\\..\\xx.pptx", "ppt-{0}.png", 0, "png", 0, 0, true, this.OnProcess);
+            res = Converter.ppt2img("xx.pptx", "ppt-{0}.png", 0, "png", 0, 0, true, this.OnProcess);
             Assert.AreEqual(0, res.Code);
             Assert.AreEqual(1, res.Count);
             res.Save("ppt1.json");
@@ -71,10 +71,10 @@ namespace io.vty.cswf.doc.test
             Assert.AreEqual(1, res.Count);
             res.Save("exec1.json");
 
-            Converter.Proc proc = new Converter.Proc("..\\..\\echo1.bat", "xx");
+            Converter.Proc proc = new Converter.Proc("echo1.bat", "xx");
             res = Converter.exec("a", "b", 0, false, proc.exec);
             Assert.AreEqual(0, res.Code);
-            Assert.AreEqual(1, res.Count);
+            Assert.AreEqual(2, res.Count);
             res.Save("exec1.json");
         }
     }

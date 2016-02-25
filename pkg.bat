ae553a9fcdefb9ca4d2d08f526c9d13a3e40d1f4
Centny <centny@gmail.com>
2016-02-25 13:20:06 +0800

    move test file to test folder.

--- pkg.bat
+++ pkg.bat
@@ -2,10 +2,12 @@
 del /Q /S build
 mkdir build
 mkdir build\cswf.doc
+mkdir build\cswf.doc\test
 ::msbuild io.vty.cswf.doc.sln /property:Configuration="Release" /t:clean /t:build
 xcopy io.vty.cswf.doc.console\bin\Release\cov_*.bat  build\cswf.doc
 xcopy io.vty.cswf.doc.console\bin\Release\cswf-doc.exe*  build\cswf.doc
 xcopy io.vty.cswf.doc.console\bin\Release\*.dll build\cswf.doc
+xcopy io.vty.cswf.doc.test\test\* build\cswf.doc\test
 
 cd build
 7z a cswf.doc.zip cswf.doc

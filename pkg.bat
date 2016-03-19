6ef9360c535d5a58e5229cc8e03cb81174a8686e
Centny <centny@gmail.com>
2016-03-20 03:33:04 +0800

    adding x64 configure.

--- pkg.bat
+++ pkg.bat
@@ -5,11 +5,11 @@ mkdir build\cswf.doc
 mkdir build\cswf.doc\sdata_i
 mkdir build\cswf.doc\sdata_i\test
 call VsMSBuildCmd
-msbuild io.vty.cswf.doc.sln /property:Configuration="Release" /t:clean /t:build
+msbuild io.vty.cswf.doc.sln /property:Configuration="Release" /p:Platform="x64" /t:clean /t:build
 if not "%errorlevel%"=="0" goto :efail
-xcopy io.vty.cswf.doc.console\bin\Release\cov_*.sh  build\cswf.doc
-xcopy io.vty.cswf.doc.console\bin\Release\cswf-doc.exe*  build\cswf.doc
-xcopy io.vty.cswf.doc.console\bin\Release\*.dll build\cswf.doc
+xcopy io.vty.cswf.doc.console\bin\x64\Release\cov_*.sh  build\cswf.doc
+xcopy io.vty.cswf.doc.console\bin\x64\Release\cswf-doc.exe*  build\cswf.doc
+xcopy io.vty.cswf.doc.console\bin\x64\Release\*.dll build\cswf.doc
 xcopy io.vty.cswf.doc.test\test\* build\cswf.doc\sdata_i\test
 
 cd build
